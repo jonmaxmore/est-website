dcdcf5153d1e6ae5db62c3a3659e054746e4685e
jonmaxmore <jonmaxmore@gmail.com>
2026-03-20 14:34:44 +0700

    fix: populate importMap for CMS analytics dashboard widget

--- src/app/(payload)/admin/[[...segments]]/importMap.js
+++ src/app/(payload)/admin/[[...segments]]/importMap.js
@@ -1,1 +1,5 @@
-export const importMap = {}
+import { default as default_fad0b14b27e56d34e3e87c4b8e94b285 } from '@/components/admin/AnalyticsDashboardWidget'
+
+export const importMap = {
+  "@/components/admin/AnalyticsDashboardWidget#default": default_fad0b14b27e56d34e3e87c4b8e94b285,
+}
